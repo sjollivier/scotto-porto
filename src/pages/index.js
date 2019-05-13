cdda0885e8157639c8b9a93a8a26891759905b02
Scott Ollivier <sollivier@sollivier-mb.local>
2019-05-13 15:10:04 -0700

    Quick edits to default starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,19 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO
+      title="Home"
+      keywords={[
+        `Scott Ollivier`,
+        `Product Management`,
+        `Product Leadership`,
+        `SaaS`,
+      ]}
+    />
+    <h1>Scott Ollivier</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
